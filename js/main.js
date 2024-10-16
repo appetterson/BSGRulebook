bc0d2585e84439638a0271166fecb11629fab024
Adam Petterson <appetterson@gmail.com>
2024-10-16 08:57:36 +0100

    Registering sw

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ if ('serviceWorker' in navigator) {
     // Wait until window is loaded before registering.
     window.addEventListener('load', () => {
       // Register the service worker with "/" as it's scope.
-      navigator.serviceWorker.register('/sw.js', { scope: '/' })
+      navigator.serviceWorker.register('/BSGRulebook/sw.js', { scope: '/BSGRulebook/' })
         // Output success/failure of registration.
         .then(() => console.log('SW registered'))
         .catch(() => console.error('SW registration failed'));
